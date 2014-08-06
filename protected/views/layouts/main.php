94974984f9e6503220e13b4b22fb853086eba425
Maxim Kutyrew (Job) <homaxweb@mail.ru>
2014-08-06 11:57:32 +0400

    Yii version

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -54,7 +54,7 @@
 	<div id="footer">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
+		<?php echo Yii::powered()." ". Yii::getVersion(); ?>
 	</div><!-- footer -->
 
 </div><!-- page -->
